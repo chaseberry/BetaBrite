9c3325630a51ee85646669f638479546cc37eaca
chase <chase@bryx.com>
2015-09-27 11:21:21 -0400

    Added basic sign codes

---
+++ src/main/kotlin/edu/csh/chase/kbetabrite/SignCodes.kt
@@ -0,0 +1,10 @@
+package edu.csh.chase.kbetabrite
+
+//TODO add the rest of these
+public enum class SignCodes(code: Char) {
+    VisualVerification('!'),
+    SerialClock('"'),
+    AlphaVisionSign('#'),
+    FullMatrixAlphaVisionSign('$'),
+    AllSigns('Z')//Lets be honest this is the only one we use
+}
