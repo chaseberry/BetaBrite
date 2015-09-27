0e31211a059341e1000b21b9c48d28d7b8c61930
chase <chase@bryx.com>
2015-09-26 21:17:09 -0400

    Made the String a Char

--- src/main/kotlin/edu/csh/chase/kbetabrite/CommandCodes.kt
+++ src/main/kotlin/edu/csh/chase/kbetabrite/CommandCodes.kt
@@ -1,22 +1,22 @@
 package edu.csh.chase.kbetabrite
 
-public enum class CommandCodes(val code: String) {
-    WriteText("A"),
-    ReadText("B"),
-    WriteSpecialFunction("E"),
-    ReadSpecialFunction("F"),
-    WriteString("G"),
-    ReadString("H"),
-    WriteSmallDots("I"),
-    ReadSmallDots("J"),
-    WriteRGBDots("K"),
-    ReadRGBDots("L"),
-    WriteLargeDots("M"),
-    ReadLargeDots("N"),
-    WriteAlphaisionBulletin("O"),
-    SetTimeout("T");
+public enum class CommandCodes(val code: Char) {
+    WriteText('A'),
+    ReadText('B'),
+    WriteSpecialFunction('E'),
+    ReadSpecialFunction('F'),
+    WriteString('G'),
+    ReadString('H'),
+    WriteSmallDots('I'),
+    ReadSmallDots('J'),
+    WriteRGBDots('K'),
+    ReadRGBDots('L'),
+    WriteLargeDots('M'),
+    ReadLargeDots('N'),
+    WriteAlphaisionBulletin('O'),
+    SetTimeout('T');
 
-    override fun toString() = code
+    override fun toString() = code.toString()
 
 
 }
