21ed33ac4c96420d0cf857c2d8dadb7b141c96bd
chase <chase@bryx.com>
2015-09-27 13:03:47 -0400

    Added teh first command

--- src/main/kotlin/edu/csh/chase/kbetabrite/commands/WriteText.kt
+++ src/main/kotlin/edu/csh/chase/kbetabrite/commands/WriteText.kt
@@ -1,17 +1,26 @@
 package edu.csh.chase.kbetabrite.commands
 
 import edu.csh.chase.kbetabrite.Command
+import edu.csh.chase.kbetabrite.File
 import edu.csh.chase.kbetabrite.models.CommandCodes
 import edu.csh.chase.kbetabrite.models.DisplayPosition
 import edu.csh.chase.kbetabrite.models.WriteModes
 import java.io.Writer
 
 public class WriteText(val text: String,
+                       val fileIndex: Int,
                        val displayPosition: DisplayPosition,
                        val displayMode: WriteModes
 
 ) : Command(CommandCodes.WriteText) {
-    override fun write(writer: Writer) {
 
+    override fun write(writer: Writer) {
+        writer.write(commandCode.code)
+        writer.write(File[fileIndex])
+        writer.write(0x1B.toString())
+        writer.write(displayPosition.code)
+        writer.write(displayMode.code)
+        writer.write(text)
     }
+    
 }
--- src/main/kotlin/edu/csh/chase/kbetabrite/models/CommandCodes.kt
+++ src/main/kotlin/edu/csh/chase/kbetabrite/models/CommandCodes.kt
@@ -1,18 +1,20 @@
 package edu.csh.chase.kbetabrite.models
 
-public enum class CommandCodes(val code: Char) {
-    WriteText('A'),
-    ReadText('B'),
-    WriteSpecialFunction('E'),
-    ReadSpecialFunction('F'),
-    WriteString('G'),
-    ReadString('H'),
-    WriteSmallDots('I'),
-    ReadSmallDots('J'),
-    WriteRGBDots('K'),
-    ReadRGBDots('L'),
-    WriteLargeDots('M'),
-    ReadLargeDots('N'),
-    WriteAlphaisionBulletin('O'),
-    SetTimeout('T')
+import edu.csh.chase.kbetabrite.Codable
+
+public enum class CommandCodes(override val code: String) : Codable {
+    WriteText("A"),
+    ReadText("B"),
+    WriteSpecialFunction("E"),
+    ReadSpecialFunction("F"),
+    WriteString("G"),
+    ReadString("H"),
+    WriteSmallDots("I"),
+    ReadSmallDots("J"),
+    WriteRGBDots("K"),
+    ReadRGBDots("L"),
+    WriteLargeDots("M"),
+    ReadLargeDots("N"),
+    WriteAlphaisionBulletin("O"),
+    SetTimeout("T")
 }
